b13fcee4867b228390161a2abe981e28a675c381
Winkie's <vincent@healsy.life>
2018-12-08 23:58:42 +0100

    refactor(emacs): init configuration file

--- emacs.d/init.el
+++ emacs.d/init.el
@@ -1,64 +1,72 @@
 ;; Emacs Configuration File
 ;; MELPA Package Emacs
-(require 'package)
 
-(add-to-list 'package-archives
-             '("melpa" . "https://melpa.org/packages/"))
-(when (< emacs-major-version 24)
-  (add-to-list 'package-archives '("gnu" . "http://elpa.gnu.org/packages/")))
+;; (require 'package)
 
+;; (add-to-list 'package-archives
+;;              '("melpa" . "https://melpa.org/packages/"))
+;; (when (< emacs-major-version 24)
+;;   (add-to-list 'package-archives '("gnu" . "http://elpa.gnu.org/packages/")))
+
+;; (package-initialize)
+
+;; ;; Install packages if they're missing
+;; (setq package-list
+;;    '(auto-complete
+;;      auto-complete-c-headers
+;;      auto-package-update
+;;      dash
+;;      company-tern
+;;      go-autocomplete
+;;      go-mode
+;;      iedit
+;;      js3-mode
+;;      linum-relative
+;;      load-dir
+;;      neotree
+;;      popup
+;;      yasnippet
+;;      zenburn-theme
+;;      ruby-mode
+;;      robe
+;;      xref-js2
+;;      yaml-mode
+;;      ))
+
+;; ;; Fetch the list of packages available
+;; (unless package-archive-contents
+;;   (package-refresh-contents))
+
+;; ;; Install the missing packages
+;; (dolist (package package-list)
+;;   (unless (package-installed-p package)
+;;         (package-install package)))
+
+;; ;; Install update package if they're pending
+;; (require 'auto-package-update)
+;; (auto-package-update-maybe)
+
+;; Leave this here, or package.el will just add it again.
 (package-initialize)
 
-;; Install packages if they're missing
-(setq package-list
-   '(auto-complete
-     auto-complete-c-headers
-     auto-package-update
-     dash
-     company-tern
-     go-autocomplete
-     go-mode
-     iedit
-     js3-mode
-     linum-relative
-     load-dir
-     neotree
-     popup
-     yasnippet
-     zenburn-theme
-     ruby-mode
-     robe
-     xref-js2
-     yaml-mode
-     ))
-
-;; Fetch the list of packages available
-(unless package-archive-contents
-  (package-refresh-contents))
-
-;; Install the missing packages
-(dolist (package package-list)
-  (unless (package-installed-p package)
-        (package-install package)))
-
-;; Install update package if they're pending
-(require 'auto-package-update)
-(auto-package-update-maybe)
-
 ;; Load my settings
-(add-to-list 'load-path "~/.emacs.d/custom")
-(require 'c-mode)
-(require 'packages)
-(require 'settings)
-(require 'messages)
+(require 'package)
+(setq package-enable-at-startup nil)
+(add-to-list 'load-path (expand-file-name "custom" user-emacs-directory))
 
-;;
-;; Yaml Mode
-;;
-(add-hook 'yaml-mode-hook
-        (lambda ()
-            (define-key yaml-mode-map "\C-m" 'newline-and-indent)))
+;; Also add all directories within "lisp"
+;; I use this for packages I'm actively working on, mostly.
+(let ((files (directory-files-and-attributes "$HOME/.emacs.d/custom" t)))
+  (dolist (file files)
+    (let ((filename (car file))
+          (dir (nth 1 file)))
+      (when (and dir
+                 (not (string-suffix-p "." filename)))
+        (add-to-list 'load-path (car file))))))
 
-
-(autoload 'js3-mode "js3" nil t)
-(add-to-list 'auto-mode-alist '("\\.js$" . js3-mode))
+;; (add-to-list 'load-path "~/.emacs.d/custom")
+;; (require 'c-mode)
+;; (require 'packages)
+;; (require 'settings)
+;; (require 'messages)
+;; (require 'go)
