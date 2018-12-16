6f5db969e602313741935e8cc8c0fb1064f0e6ae
Winkie's <vincent@healsy.life>
2018-12-16 20:04:53 +0100

    refactor(i3): rename lock picture and clean-up i3lock shell script

--- i3/i3lock.sh
+++ i3/i3lock.sh
@@ -1,9 +1,4 @@
 #!/bin/bash
 
-# Pixellate it 10x
-
-#mogrify -scale 10% -scale 1000% ~/Pictures/watchment.png
-
-# Lock screen displaying this image.
-
-i3lock -i  ~/Pictures/watchmen.png
+# Lock screen displaying this image
+i3lock -i  ~/Pictures/lock.png
